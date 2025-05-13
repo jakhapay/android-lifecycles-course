72782090402955740170cdac96a35fb91e6e39f2
Jakhongir Mannonov <jakhongir.m@payme.uz>
2025-05-13 09:31:41 +0500

    AGP issues fixed

--- app/src/main/java/com/techyourchance/androidlifecycles/saveandrestore/SaveAndRestoreActivity.kt
+++ app/src/main/java/com/techyourchance/androidlifecycles/saveandrestore/SaveAndRestoreActivity.kt
@@ -221,7 +221,7 @@ private class MyViewModelFactory(
     owner: SavedStateRegistryOwner,
 ) : AbstractSavedStateViewModelFactory(owner, null) {
 
-    override fun <T : ViewModel?> create(
+    override fun <T : ViewModel> create(
         key: String,
         modelClass: Class<T>,
         handle: SavedStateHandle
